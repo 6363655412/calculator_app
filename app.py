6634c0969713b0fdd26fe9f7e8770f30ff56918b
6363655412 <priyanka.ai.work@gmail.com>
2024-04-24 21:38:38 +0530

    mul operation

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from cal_func import do_addition
 from cal_func import do_sub
+from mul import mul
 def main():
     print('''Welcome calculator app..
           1.add
-          2.sub''')
+          2.sub
+          3.mul''')
 
     user_input = input("select the function")
     a = int(input("value of A"))
     b = int(input("value of B"))
+
     result = 0
     if user_input == "1":
         result =  do_addition(a,b)
     elif user_input == "2":
         result = do_sub(a,b)
+    elif user_input == "3":
+        result =  mul(a,b)
     print('Result:',result)
 
 if __name__ == "__main__":
