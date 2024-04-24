f02d082a8058f8470fde99a7f9d550c8de0259a2
6363655412 <priyanka.ai.work@gmail.com>
2024-04-24 21:18:47 +0530

    Intialize done

---
+++ app.py
@@ -0,0 +1,22 @@
+from cal_func import do_addition
+from cal_func import do_sub
+def main():
+    print('''Welcome calculator app..
+          1.add
+          2.sub''')
+
+    user_input = input("select the function")
+    a = int(input("value of A"))
+    b = int(input("value of B"))
+    result = 0
+    if user_input == "1":
+        result =  do_addition(a,b)
+    elif user_input == "2":
+        result = do_sub(a,b)
+    print('Result:',result)
+
+if __name__ == "__main__":
+    main()
+
+
+
